249e584cfad0391b11676219f8115b1549b1a369
soizhiwen <zhiwen39@gmail.com>
2025-10-16 13:30:37 +0000

    Upgrade to latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://soizhiwen.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
