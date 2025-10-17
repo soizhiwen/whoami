b0e94f275fb2f34c8f74b5bdcc6e52cbb3b1a0c2
soizhiwen <zhiwen39@gmail.com>
2025-10-17 13:55:16 +0000

    Update TimeWak

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,20 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 import react from '@astrojs/react';
+import expressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://soizhiwen.com',
-  integrations: [mdx(), sitemap(), icon(), react()],
+  integrations: [expressiveCode({
+    themes: ['github-light'],
+    styleOverrides: {
+      width: '100%',
+      frames: {
+        shadowColor: 'transparent',
+      }
+    }
+  }), mdx(), sitemap(), icon(), react()],
   vite: {
     plugins: [tailwindcss()]
   }
