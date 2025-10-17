f747e034105e5c5de68bf5ebd358f652a32a5fd6
soizhiwen <zhiwen39@gmail.com>
2025-10-17 08:51:46 +0000

    Add academic template

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://soizhiwen.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon(), react()],
   vite: {
     plugins: [tailwindcss()]
   }
